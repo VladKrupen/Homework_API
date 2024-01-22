a51aa885690bc511a0d0c509eb0ba2f500fd9d13
Vlad Krupen <air7360@gmail.com>
2024-01-22 20:30:39 +0300

    Initial commit

---
+++ Homework_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_API
+//
+//  Created by Vlad on 22.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
