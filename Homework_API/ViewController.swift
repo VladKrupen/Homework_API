3fb94b3d915f10ae64ef10841e1b2058493ef1e1
Vlad Krupen <air7360@gmail.com>
2024-01-22 21:42:32 +0300

    getting objects JSON

--- Homework_API/ViewController.swift
+++ Homework_API/ViewController.swift
@@ -11,7 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+        let urlRequest = URLRequest(url: url)
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            let dict = try! JSONSerialization.jsonObject(with: data!)
+            print(dict)
+            
+        }.resume()
+
     }
 
 
