8d2a941847c4d1adb3627bfc46191a3c731be16c
Vlad Krupen <air7360@gmail.com>
2024-01-24 20:22:02 +0300

    all JSON elements added

--- Homework_API/ViewController.swift
+++ Homework_API/ViewController.swift
@@ -7,31 +7,71 @@
 
 import UIKit
 
-struct User: Codable {
-    let name: String
-    let email: String
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.name, forKey: .name)
-        try container.encode(self.email, forKey: .email)
-    }
+// MARK: - ToDo
+struct ToDo: Codable {
+    let id: Int
+    let name, username, email: String
+    let address: Address
+    let phone, website: String
+    let company: Company
     
     enum CodingKeys: CodingKey {
+        case id
         case name
+        case username
         case email
+        case address
+        case phone
+        case website
+        case company
+
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.id, forKey: .id)
+        try container.encode(self.name, forKey: .name)
+        try container.encode(self.username, forKey: .username)
+        try container.encode(self.email, forKey: .email)
+        try container.encode(self.address, forKey: .address)
+        try container.encode(self.phone, forKey: .phone)
+        try container.encode(self.website, forKey: .website)
+        try container.encode(self.company, forKey: .company)
+
     }
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
         self.name = try container.decode(String.self, forKey: .name)
+        self.username = try container.decode(String.self, forKey: .username)
         self.email = try container.decode(String.self, forKey: .email)
+        self.address = try container.decode(Address.self, forKey: .address)
+        self.phone = try container.decode(String.self, forKey: .phone)
+        self.website = try container.decode(String.self, forKey: .website)
+        self.company = try container.decode(Company.self, forKey: .company)
     }
+}
+
+// MARK: - Address
+struct Address: Codable {
+    let street, suite, city, zipcode: String
+    let geo: Geo
+}
+
+// MARK: - Geo
+struct Geo: Codable {
+    let lat, lng: String
+}
+
+// MARK: - Company
+struct Company: Codable {
+    let name, catchPhrase, bs: String
 }
 
 class ViewController: UIViewController {
     
-    var arrayUsers: [User] = []
+    var arrayUsers: [ToDo] = []
     
     let tableView = {
         let tableView = UITableView()
@@ -55,9 +95,9 @@ class ViewController: UIViewController {
             let dict = try! JSONSerialization.jsonObject(with: data!)
             print(dict)
             
-            let users = try! JSONDecoder().decode([User].self, from: data!)
-            
-            self.arrayUsers = users
+            let todo = try! JSONDecoder().decode([ToDo].self, from: data!)
+
+            self.arrayUsers = todo
             
             DispatchQueue.main.async {
                 self.tableView.reloadData()
